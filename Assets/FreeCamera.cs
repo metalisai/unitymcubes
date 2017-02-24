38e1d8898fab89ca75e9d2e9b1271720cee7b78a
Talis Tammearu <talis995@gmail.com>
2017-02-24 19:05:25 +0200

    ,

---
+++ Assets/FreeCamera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class FreeCamera : MonoBehaviour
+{
+    public float MoveSpeed = 10.0f;
+    float _rotX = 0.0f;
+    float _rotY = 0.0f;
+
+    void LateUpdate()
+    {
+        Transform camtrans = Camera.main.transform;
+        if (Input.GetKey(KeyCode.W))
+            camtrans.position += camtrans.forward * Time.deltaTime * MoveSpeed;
+        if (Input.GetKey(KeyCode.S))
+            camtrans.position -= camtrans.forward * Time.deltaTime * MoveSpeed;
+
+        _rotX += Input.GetAxis("Mouse X");
+        _rotY += Input.GetAxis("Mouse Y");
+
+        camtrans.rotation = Quaternion.AngleAxis(_rotX, Vector3.up)* Quaternion.AngleAxis(_rotY, -Vector3.right);
+    }
+}
---
+++ Assets/TerrainGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    MarchingCubes _mcubes = new MarchingCubes();
+    const float voxelSize = 1.0f;
+
+    void Start()
+    {
+        int chunkSize = 10;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                GenerateChunk(new Vector3(i*voxelSize*chunkSize, 0.0f, j*voxelSize*chunkSize), chunkSize);
+            }
+        }
+    }
+
+    GameObject GenerateChunk(Vector3 origin, int size)
+    {
+        _mcubes.Reset();
+        for(int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    _mcubes.MarchCube(origin.x + i * voxelSize, origin.y + j * voxelSize, origin.z + k * voxelSize, voxelSize);
+                }
+            }
+        }
+        var mesh = new Mesh();
+        mesh.vertices = _mcubes.GetVertices();
+        mesh.triangles = _mcubes.GetIndices();
+        mesh.uv = new Vector2[mesh.vertices.Length];
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        var go = new GameObject("TerrainChunk");
+        var mf = go.AddComponent<MeshFilter>();
+        var mr = go.AddComponent<MeshRenderer>();
+        mf.mesh = mesh;
+        mr.material = new Material(Shader.Find("Standard"));
+        return go;
+    }
+}
